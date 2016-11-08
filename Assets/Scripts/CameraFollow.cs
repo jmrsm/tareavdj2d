fa6a009ca1f4c47f9d90effc11558a9e9fe885b1
jmrsm <jmrsm01@gmail.com>
2016-11-08 04:10:58 -0300

    Arreglos en la camara y en el entorno

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ public class CameraFollow : MonoBehaviour {
 	public Transform target;
 
 	private Vector3 targetPosition;
-
+	private int targetPositionY;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,8 +13,15 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/*Camera camera = GetComponent<Camera> ();
+		//Vector3 p = camera.ScreenToWorldPoint (this.target.position);
+		//Gizmos.color = Color.yellow;*/
+		//Gizmos.DrawSphere (p, 0.1F);
 		this.targetPosition = this.target.position;
 		this.targetPosition.z = -10;
+		//Seteamos la camara para que quede fija
+		this.targetPosition.y = -1.0f;
 		this.transform.position = Vector3.Lerp (this.transform.position, this.targetPosition, Time.deltaTime * 2);
+
 	}
 }
